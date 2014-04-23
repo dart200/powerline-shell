2c91c07326f707edd17697a9cf34a01cd5443c42
Nicholas Swenson <nks@daterainc.com>
2014-04-23 11:40:02 -0700

    styling hostname combined with sms

--- segments/hostname.py
+++ segments/hostname.py
@@ -6,23 +6,22 @@ def add_hostname_segment():
         hostname = gethostname()
         FG, BG = stringToHashToColorAndOpposite(hostname)
         FG, BG = (rgb2short(*color) for color in [FG, BG])
-        host_prompt = ' %s' % hostname.split('.')[0]
+        host_prompt = ' %s ' % hostname.split('.')[0]
 
         powerline.append(host_prompt, FG, BG)
     else:
         if powerline.args.shell == 'bash':
-            host_prompt = ' \\h '
+            host_prompt = ' \\h'
         elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m '
+            host_prompt = ' %m'
         else:
             import socket
-            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
+            host_prompt = ' %s' % socket.gethostname().split('.')[0]
 
 	if os.environ.get("SMSCHROOT"):
-	    powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG,
-			     powerline.separator_thin, Color.SEPARATOR_FG)
-	else:
 	    powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
+	else:
+	    powerline.append(host_prompt + ' ', Color.HOSTNAME_FG, Color.HOSTNAME_BG)
 
 
 add_hostname_segment()
--- segments/sms.py
+++ segments/sms.py
@@ -1,6 +1,6 @@
 def add_sms_segment():
     if os.environ.get('SMSCHROOT'):
-        host_prompt = ' %s ' % os.environ.get("SMSCHROOT").split("/")[-1]
+        host_prompt = '%s ' % os.environ.get("SMSCHROOT").split("/")[-1]
         powerline.append(host_prompt, Color.SMS_FG, Color.SMS_BG)
 
 add_sms_segment()
